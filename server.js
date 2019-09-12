392ae2a5c848f36419d47cde02037c0a2baea666
Renan Ramos <renanrramossi@gmail.com>
2019-09-12 11:28:00 -0300

    Adequação do projeto para deploy no Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Somente arquivos estáticos
+app.use(express.static(__dirname + '/dist/todo-angular-application'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/todo-angular-application/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
